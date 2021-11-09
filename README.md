c361581d319471f7e915f46dc16c71cba207cb8a
raoqidi <raoqidi@rongshantech.com>
2021-11-09 10:22:16 +0800

    feat:补充readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # qiankun-demo
-qiankun框架demo搭建
+
+qiankun 框架 demo 搭建
+
+vite 需要单独启动
+
+```bash
+先进行打包操作
+yarn build
+
+在跑项目
+yarn serve
+```
--- clone-all.sh
+++ clone-all.sh
@@ -2,7 +2,7 @@
  # @Author: raoqidi
  # @Date: 2021-08-23 17:43:32
  # @LastEditors: raoqidi
- # @LastEditTime: 2021-08-23 17:44:06
+ # @LastEditTime: 2021-11-09 10:12:52
  # @Description: please add a description to the file
  # @FilePath: /qiankun-demo/clone-all.sh
 ### 
@@ -11,5 +11,7 @@
 # git clone git@vue2.git
 
 # vue3
+# git clone git@vue3.git
 
 # react17
+# git clone git@react17.git
